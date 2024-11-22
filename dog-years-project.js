f751c42f43307304828ebd68b8d4a577b8c169ee
Nickisew <nicole.sewell@outlook.com>
2024-11-22 17:08:59 -0500

    first commit

---
+++ dog-years-project.js
@@ -0,0 +1,23 @@
+// Dog Years
+
+const myAge = 27;
+
+var earlyYears = 2;
+
+earlyYears *= earlyYears;
+
+laterYears = myAge - 2;
+
+laterYears = laterYears * 4;
+
+var myAgeInDogYears = earlyYears + laterYears;
+
+var originalString = "NICOLE";
+
+var myName = originalString.toLowerCase();
+
+console.log(
+  `My name is ${myName}. I am ${myAge} in human years which is ${myAgeInDogYears} years old in dog years.`
+);
+
+console.log(`Nicole`);
